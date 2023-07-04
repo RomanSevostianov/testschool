472dfe7ae4e8047c395473e83d3b6fccd39d04e7
Роман <pangujailbreake58@gmail.com>
2023-07-04 21:09:46 +0300

    1

---
+++ src/test/java/ru/schooltest/testschool/TestSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.schooltest.testschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
